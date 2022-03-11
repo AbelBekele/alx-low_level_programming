0cac678c9c3c66869ed809613ace2e7741898321
Abel Bekele <abelbekele.addise@gmail.com>
2022-03-11 11:14:24 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	char (c);
-		for (c = 'a'; c <= 'z'; ++c)
-			printf("%c", c);
-		for (c = 'A'; c <= 'Z'; ++c)
-			printf("%c", c);
+	for (c = 'a'; c <= 'z'; ++c)
+		printf("%c", c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		printf("%c", c);
 	printf("\n");
 	return (0);
-}	
+}
