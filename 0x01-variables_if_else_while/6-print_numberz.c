b07fd87742c0a346d2b6671e6ac297e12b13e5af
Abel Bekele <abelbekele.addise@gmail.com>
2022-03-11 11:53:50 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int (c);
+	for (c = 0; c < 10; ++c)
+		putchar(c + '0');
+	putchar('\n');
+	return (0);
+}
