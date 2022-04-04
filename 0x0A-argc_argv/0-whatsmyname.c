f287c619cd0ec949692a518714de2509dfe66247
Abel Bekele <abelbekele.addise@gmail.com>
2022-04-04 07:08:28 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -12,7 +13,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i = argc - 1;
 
 	printf("%s\n", argv[i]);
 	return (0);
